3ffe0e3509331da96975bb07841da0f6dd2d16da
Isabela Rabasa <isabelarabasa@Isabelas-MacBook-Pro.local>
2019-03-25 22:34:51 -0600

    not finish

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//pseudo coding
+
+
+//a game with for crystals and a random result
+//each crystal with have a random number between 1-12
+// a new random number should be regeneate every single time we win or lose to those 4 crystals
+//when clicking any crystal it should be adding with the previous result until it equals the total score 
+// if it is not equal then restart(we start over) and increiment the lost score
+//if it is equal, then increment 1 the win score
