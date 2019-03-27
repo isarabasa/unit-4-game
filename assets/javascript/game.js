39f2c6cab0b3364b03756c027d3fa03e620cfe76
Isabela Rabasa <isabelarabasa@Isabelas-MacBook-Pro.local>
2019-03-27 15:58:13 -0600

    almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,75 @@
-//pseudo coding
+$(document).ready(function() {
 
-
-//a game with for crystals and a random result
-//each crystal with have a random number between 1-12
-// a new random number should be regeneate every single time we win or lose to those 4 crystals
-//when clicking any crystal it should be adding with the previous result until it equals the total score 
-// if it is not equal then restart(we start over) and increiment the lost score
-//if it is equal, then increment 1 the win score
+    var wins = 0;
+    var losses = 0;
+    var currentScore = 0;
+    var getRandomNumber = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      };
+    
+      var randomNumber = getRandomNumber(19, 120);
+    
+      $("#randomNumber").html(randomNumber);
+    
+      var greenCrystal = getRandomNumber(1, 12);
+      var yellowCrystal = getRandomNumber(1, 12);
+      var blueCrystal = getRandomNumber(1, 12);
+      var redCrystal = getRandomNumber(1, 12);
+    
+      var newValues = function() {
+        greenCrystal = getRandomNumber(1, 12);
+        yellowCrystal = getRandomNumber(1, 12);
+        blueCrystal = getRandomNumber(1, 12);
+        redCrystal = getRandomNumber(1, 12);
+      };
+    
+    
+      $("#green-crystal").on("click", function() {
+        currentScore += greenCrystal;
+        $("#currentScore").html(currentScore);
+        updateScore();
+      });
+    
+      $("#yellow-crystal").on("click", function() {
+        currentScore += yellowCrystal;
+        $("#currentScore").html(currentScore);
+        updateScore();
+      });
+    
+      $("#blue-crystal").on("click", function() {
+        currentScore += blueCrystal;
+        $("#currentScore").html(currentScore);
+        updateScore();
+      });
+    
+      $("#red-crystal").on("click", function() {
+        currentScore += redCrystal;
+        $("#currentScore").html(currentScore);
+        updateScore();
+      });
+    
+      var updateScore = function() {
+        if (randomNumber === currentScore) {
+          wins++;
+          $("#wins").html(wins);
+          $("#randomNumber").empty();
+          randomNumber = getRandomNumber(19,120);
+          $("#randomNumber").html(randomNumber);
+          currentScore = 0;
+          $("#currentScore").html(currentScore)
+          newValues();
+        }
+        if (randomNumber < currentScore) {
+          losses++;
+          $("#losses").html(losses);
+          $("#randomNumber").empty();
+          randomNumber = getRandomNumber(19,120);
+          $("#randomNumber").html(randomNumber);
+          currentScore = 0;
+          $("#currentScore").html(currentScore)
+          newValues();
+        }
+    
+      }
+    
+    });
